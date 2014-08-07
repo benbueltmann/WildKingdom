895ce72da63df512b5ebb0105fc95552613cd259
benbueltmann <benbueltmann@gmail.com>
2014-08-07 14:56:32 -0500

    Initial Commit

---
+++ WildKingdom/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WildKingdom
+//
+//  Created by Ben Bueltmann on 8/7/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
